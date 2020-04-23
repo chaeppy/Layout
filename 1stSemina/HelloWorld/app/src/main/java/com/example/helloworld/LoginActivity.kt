d09f0f70915ce163d0c53f1c9d6892a8d45a6019
chaeppy <chaeppy108@gmail.com>
2020-04-23 20:16:11 +0900

    [basic project2] login and register

---
+++ 1stSemina/HelloWorld/app/src/main/java/com/example/helloworld/LoginActivity.kt
@@ -0,0 +1,37 @@
+package com.example.helloworld
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        txt_register.setOnClickListener{
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivityForResult(intent,101)
+        }
+
+        btn_login.setOnClickListener{
+            if(et_email.text.isNullOrBlank()||et_pwd.text.isNullOrBlank())
+                Toast.makeText(this,"아이디와 비밀번호를 확인하세요",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 101){
+            val email = data?.getStringExtra("email")
+            val pwd = data?.getStringExtra("pwd")
+
+            if(!email.isNullOrEmpty() && !pwd.isNullOrEmpty()){
+                et_email.setText(email.toString())
+                et_pwd.setText(pwd.toString())
+            }
+        }
+    }
+}
