d535eb04cada7f16f1f24d55584a608f6c3654e3
chaeppy <chaeppy108@gmail.com>
2020-06-02 04:22:40 +0900

    [성장과제2] SharedPreferences

--- HelloWorld/app/src/main/java/com/example/helloworld/LoginActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/LoginActivity.kt
@@ -1,17 +1,27 @@
 package com.example.helloworld
 
+import android.R.attr.key
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        /**sharedPreferences 데이터 로드**/
+        val prefs = this.getSharedPreferences("prefs",0)
+        et_email.setText(prefs.getString("email", ""))
+        et_pwd.setText(prefs.getString("password", ""))
+
         txt_register.setOnClickListener{
             val intent = Intent(this,RegisterActivity::class.java)
             startActivityForResult(intent,101)
@@ -37,6 +47,13 @@ class LoginActivity : AppCompatActivity() {
             if(!email.isNullOrEmpty() && !pwd.isNullOrEmpty()){
                 et_email.setText(email.toString())
                 et_pwd.setText(pwd.toString())
+
+                /**sharedPreferences 데이터 저장**/
+                val prefs = this.getSharedPreferences("prefs",0)
+                val editor = prefs.edit()
+                editor.putString("email", email.toString()) //putBoolean, putInt, putLong 등 데이터타입에 따라 사용
+                editor.putString("pwd", pwd.toString())
+                editor.commit()
             }
         }
     }
